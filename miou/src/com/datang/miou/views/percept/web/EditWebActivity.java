49cf52796d12a11de8c8f2b0278f78483c82e021
dingzhongchang <zhongchang.ding@gmail.com>
2015-03-20 14:03:59 +0800

    merge

--- miou/src/com/datang/miou/views/percept/web/EditWebActivity.java
+++ miou/src/com/datang/miou/views/percept/web/EditWebActivity.java
@@ -1,34 +1,28 @@
-package com.datang.miou.views.percept.video;
+package com.datang.miou.views.percept.web;
 
 import android.app.Activity;
-import android.graphics.PixelFormat;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.MediaController;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.datang.miou.ActivitySupport;
 import com.datang.miou.R;
-import com.datang.miou.annotation.AfterView;
 import com.datang.miou.annotation.AutoView;
 
 /**
- * 视频测试
- * Created by dingzhongchang on 2015/3/16.
+ * Created by dingzhongchang on 2015/3/18.
  */
-@AutoView(R.layout.video_activity)
-public class VideoActivity extends ActivitySupport {
-
-
+@AutoView(R.layout.edit_web_activity)
+public class EditWebActivity extends ActivitySupport {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView mTitleTextView = (TextView) findViewById(R.id.app_title_value);
-        mTitleTextView.setText("视频测试");
+        mTitleTextView.setText("网页测试");
+        TextView mRight = (TextView) findViewById(R.id.app_title_right_txt);
+        mRight.setText("完成");
         ImageView mBackButton = (ImageView) findViewById(R.id.app_title_left);
         mBackButton.setOnClickListener(new View.OnClickListener() {
 
@@ -43,19 +37,11 @@ public class VideoActivity extends ActivitySupport {
                 }
             }
         });
-    }
-
-    @AfterView
-    private void init() {
-
-        VideoView mVv = (VideoView) findViewById(R.id.vv_video);
-        getWindow().setFormat(PixelFormat.TRANSLUCENT);
-        MediaController mc = new MediaController(this);
-        mc.setMediaPlayer(mVv);
-        mVv.setMediaController(mc);
-        mVv.setVideoURI(Uri.parse("http://static.youku.com/v1.0.0512/v/swf/loader.swf"));
-        mVv.requestFocus();
-        mVv.start();
-
+        mRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
