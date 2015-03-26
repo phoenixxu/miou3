50f781a127a86bb4f406b00fe17329cd598f3d8e
dingzhongchang <zhongchang.ding@gmail.com>
2015-03-26 14:02:20 +0800

    新增视频测试

--- miou/src/com/datang/miou/views/percept/video/VideoActivity.java
+++ miou/src/com/datang/miou/views/percept/video/VideoActivity.java
@@ -1,15 +1,19 @@
 package com.datang.miou.views.percept.video;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
-import android.graphics.PixelFormat;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.PluginState;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.MediaController;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.datang.miou.ActivitySupport;
 import com.datang.miou.R;
@@ -17,45 +21,110 @@ import com.datang.miou.annotation.AfterView;
 import com.datang.miou.annotation.AutoView;
 
 /**
- * 视频测试
- * Created by dingzhongchang on 2015/3/16.
+ * 视频测试 Created by dingzhongchang on 2015/3/16.
  */
 @AutoView(R.layout.video_activity)
 public class VideoActivity extends ActivitySupport {
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView mTitleTextView = (TextView) findViewById(R.id.app_title_value);
-        mTitleTextView.setText("视频测试");
-        ImageView mBackButton = (ImageView) findViewById(R.id.app_title_left);
-        mBackButton.setOnClickListener(new View.OnClickListener() {
+  private WebView webview;
+  private Button videoCtl;
 
-            @Override
-            public void onClick(View view) {
-                try {
-                    if (NavUtils.getParentActivityName((Activity) getApplicationContext()) != null) {
-                        NavUtils.navigateUpFromSameTask((Activity) getApplicationContext());
-                    }
-                } catch (Exception e) {
-                    finish();
-                }
-            }
-        });
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    TextView mTitleTextView = (TextView) findViewById(R.id.app_title_value);
+    mTitleTextView.setText("视频测试");
+    ImageView mBackButton = (ImageView) findViewById(R.id.app_title_left);
+    mBackButton.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View view) {
+        try {
+          if (NavUtils.getParentActivityName((Activity) getApplicationContext()) != null) {
+            NavUtils.navigateUpFromSameTask((Activity) getApplicationContext());
+          }
+        } catch (Exception e) {
+          finish();
+        }
+      }
+    });
+  }
+
+  @AfterView
+  private void init() {
+
+    // VideoView mVv = (VideoView) findViewById(R.id.vv_video);
+    // getWindow().setFormat(PixelFormat.TRANSLUCENT);
+    // MediaController mc = new MediaController(this);
+    // mc.setMediaPlayer(mVv);
+    // mVv.setMediaController(mc);
+    // mVv.setVideoURI(Uri.parse("http://static.youku.com/v1.0.0512/v/swf/loader.swf"));
+    // mVv.requestFocus();
+    // mVv.start();
+
+    webview = (WebView) this.findViewById(R.id.vv_webview);
+    // get the WebView's instance
+    // 设置WebView属性,能够执行Javascript脚本
+    webview.getSettings().setJavaScriptEnabled(true);
+    WebSettings settings = webview.getSettings();
+    // settings.setPluginsEnabled(true);
+    settings.setJavaScriptEnabled(true);
+    settings.setPluginState(PluginState.ON);
+    settings.setJavaScriptCanOpenWindowsAutomatically(true);
+    settings.setAllowFileAccess(true);
+    // settings.setPluginsEnabled(true);
+    settings.setDefaultTextEncodingName("UTF-8");
+    webview.setBackgroundColor(0);
+    webview.setVisibility(View.VISIBLE);
+    // 加载指定url的网页
+    // mywebview.loadUrl("http://player.youku.com/player.php/Type/Folder/Fid/18443222/Ob/1/sid/XNDY1ODQ0MjU2/v.swf");
+    webview.loadUrl("http://www.youku.com");
+    webview.setWebViewClient(new WebViewClient() {
+      public boolean shouldOverrideUrlLoading(WebView view, String url) { // 重写此方法表明点击网页里面的链接还是在当前的webview里跳转，不跳到浏览器那边
+        view.loadUrl(url);
+        return true;
+      }
+
+    });
+
+    videoCtl = (Button) this.f(R.id.bt_video_ctl);
+    videoCtl.setOnClickListener(new View.OnClickListener() {
+      @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+      @Override
+      public void onClick(View v) {
+        if (videoCtl.getText().equals("停止测试")) {
+          videoCtl.setText("开始测试");
+          webview.onPause();
+        } else {
+          videoCtl.setText("停止测试");
+          webview.onResume();
+        }
+      }
+    });
+  }
+
+  @Override
+  protected void onResume() {
+    webview.onResume();
+    videoCtl.setText("停止测试");
+    super.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    webview.onPause();
+    videoCtl.setText("开始测试");
+    super.onPause();
+  }
+
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+      // goBack()返回WebView的上一层页面
+      webview.goBack();
+      return true;
     }
-
-    @AfterView
-    private void init() {
-
-        VideoView mVv = (VideoView) findViewById(R.id.vv_video);
-        getWindow().setFormat(PixelFormat.TRANSLUCENT);
-        MediaController mc = new MediaController(this);
-        mc.setMediaPlayer(mVv);
-        mVv.setMediaController(mc);
-        mVv.setVideoURI(Uri.parse("http://static.youku.com/v1.0.0512/v/swf/loader.swf"));
-        mVv.requestFocus();
-        mVv.start();
-
-    }
+    return super.onKeyDown(keyCode, event);
+  }
 }
--- miou/src/com/datang/miou/views/percept/web/WebActivity.java
+++ miou/src/com/datang/miou/views/percept/web/WebActivity.java
@@ -19,84 +19,103 @@ import com.datang.miou.annotation.AutoView;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * 网页测试
- * Created by dingzhongchang on 2015/3/16.
+ * 网页测试 Created by dingzhongchang on 2015/3/16.
  */
 @AutoView(R.layout.web_activity)
 public class WebActivity extends ActivitySupport {
 
-    AtomicBoolean isStop = new AtomicBoolean(false);
+  AtomicBoolean isStop = new AtomicBoolean(false);
+  private Button webCtl;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView mTitleTextView = (TextView) findViewById(R.id.app_title_value);
-        mTitleTextView.setText("网页测试");
-        TextView mRight = (TextView) findViewById(R.id.app_title_right_txt);
-        mRight.setText("编辑");
-        ImageView mBackButton = (ImageView) findViewById(R.id.app_title_left);
-        mBackButton.setOnClickListener(new View.OnClickListener() {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    TextView mTitleTextView = (TextView) findViewById(R.id.app_title_value);
+    mTitleTextView.setText("网页测试");
+    TextView mRight = (TextView) findViewById(R.id.app_title_right_txt);
+    mRight.setText("编辑");
+    ImageView mBackButton = (ImageView) findViewById(R.id.app_title_left);
+    mBackButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                try {
-                    if (NavUtils.getParentActivityName((Activity) getApplicationContext()) != null) {
-                        NavUtils.navigateUpFromSameTask((Activity) getApplicationContext());
-                    }
-                } catch (Exception e) {
-                    finish();
-                }
-            }
-        });
-        mRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(mContext, EditWebActivity.class));
-            }
-        });
-    }
+      @Override
+      public void onClick(View view) {
+        try {
+          if (NavUtils.getParentActivityName((Activity) getApplicationContext()) != null) {
+            NavUtils.navigateUpFromSameTask((Activity) getApplicationContext());
+          }
+        } catch (Exception e) {
+          finish();
+        }
+      }
+    });
+    mRight.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        startActivity(new Intent(mContext, EditWebActivity.class));
+      }
+    });
+  }
 
-    @AfterView
-    private void init() {
-        final ProgressBar baidu = (ProgressBar) this.f(R.id.pb_baidu);
-        final ProgressBar sina = (ProgressBar) this.f(R.id.pb_sina);
-        final ProgressBar mobile = (ProgressBar) this.f(R.id.pb_mobile);
-        final ProgressBar tengxu = (ProgressBar) this.f(R.id.pb_tengxu);
-        final ProgressBar youku = (ProgressBar) this.f(R.id.pb_youku);
-        new Thread(new Runnable() {
+  @AfterView
+  private void init() {
+    final ProgressBar baidu = (ProgressBar) this.f(R.id.pb_baidu);
+    final ProgressBar sina = (ProgressBar) this.f(R.id.pb_sina);
+    final ProgressBar mobile = (ProgressBar) this.f(R.id.pb_mobile);
+    final ProgressBar tengxu = (ProgressBar) this.f(R.id.pb_tengxu);
+    final ProgressBar youku = (ProgressBar) this.f(R.id.pb_youku);
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        int progress = 0;
+        while (true) {
+          final int finalProgress = progress;
+          WebActivity.this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                int progress = 0;
-                while (true) {
-                    final int finalProgress = progress;
-                    WebActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            baidu.setProgress(finalProgress);
-                            sina.setProgress(finalProgress);
-                            mobile.setProgress(finalProgress);
-                            tengxu.setProgress(finalProgress);
-                            youku.setProgress(finalProgress);
-                        }
-                    });
-                    SystemClock.sleep(1000);
-                    if (progress++ == 100 || isStop.get()) break;
-                }
+              baidu.setProgress(finalProgress);
+              sina.setProgress(finalProgress);
+              mobile.setProgress(finalProgress);
+              tengxu.setProgress(finalProgress);
+              youku.setProgress(finalProgress);
             }
-        }).start();
+          });
+          SystemClock.sleep(1000);
+          if (progress++ == 100 || isStop.get())
+            break;
+        }
+      }
+    }).start();
 
-        final Button webCtl = (Button) this.f(R.id.bt_web_ctl);
-        webCtl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!isStop.get()) {
-                    isStop.set(true);
-                    webCtl.setText("开始测试");
-                } else {
-                    isStop.set(false);
-                    webCtl.setText("停止测试");
-                }
-            }
-        });
+    webCtl = (Button) this.f(R.id.bt_web_ctl);
+    webCtl.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        ctl();
+      }
+    });
+  }
+
+  @Override
+  protected void onResume() {
+    isStop.set(false);
+    webCtl.setText("停止测试");
+    super.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    isStop.set(true);
+    webCtl.setText("开始测试");
+    super.onPause();
+  }
+
+  private void ctl() {
+    if (!isStop.get()) {
+      isStop.set(true);
+      webCtl.setText("开始测试");
+    } else {
+      isStop.set(false);
+      webCtl.setText("停止测试");
     }
+  }
 }
